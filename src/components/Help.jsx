10bd3a128a14f06d1994a7992f7129983e0f4da8
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-10-24 09:49:43 -0300

    Add multiple collapses

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -6,7 +6,19 @@ import "react-sliding-pane/dist/react-sliding-pane.css";
 
 function Help() {
     const [openPanel, setOpenPanel] = useState(false);
-    const [showCollapse, setShowCollapse] = useState(false);
+    const [showCollapse, setShowCollapse] = useState([false, false, false, false, false, false, false, false, false, false, false]);
+
+    const handleCollapse = (index) => {
+        let arr_collapse = showCollapse.map((valor, i) => {
+            if (index == i) {
+                return !valor;
+            } else {
+                return valor;
+            }
+        });
+
+        setShowCollapse(arr_collapse);
+    }
 
     return (
         <div className="help-panel">
@@ -21,12 +33,92 @@ function Help() {
                 width="45%"
                 onRequestClose={() => setOpenPanel(false)}
             >
-                <div className="row" onClick={() => setShowCollapse(!showCollapse)}>
-                    <i class="fa-solid fa-angle-right"></i>
+                <div className="row" onClick={() => handleCollapse(0)}>
+                    <i className="fa-solid fa-angle-right"></i>
                     Sobre o jogo
                 </div>
-                <Collapse isOpen={showCollapse}>
-                    <div>Teste</div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[0]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(1)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    O que é SQL?
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[1]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(2)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Estrutura de comandos
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[2]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(3)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Selecionar todos os dados de uma tabela (SELECT)
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[3]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(4)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Selecionar colunas específicas
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[4]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(5)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Ordenar linhas (ORDER BY)
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[5]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(6)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Filtrar linhas (WHERE)
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[6]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(7)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Operadores condicionais (AND, OR)
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[7]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(8)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Atualizar linhas (UPDATE)
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[8]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(9)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Dicas
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[9]}>
+                    <div className="content-collapse">...</div>
+                </Collapse>
+
+                <div className="row" onClick={() => handleCollapse(10)}>
+                    <i className="fa-solid fa-angle-right"></i>
+                    Saiba mais
+                </div>
+                <Collapse transition={`height 290ms cubic-bezier(.4, 0, .2, 1)`} isOpen={showCollapse[10]}>
+                    <div className="content-collapse">...</div>
                 </Collapse>
 
             </SlidingPane>
